09bd6cd398342de275d0f61c7b88bbfd125fd1f4
isaacgit91 <isaac__ps4@hotmail.com>
2025-11-26 16:30:48 +0000

    Initial commit

---
+++ Assets/Scripts/Obstacle.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour
+{
+
+    public float minSize = 0.5f;
+    public float maxSize = 2.0f;
+    Rigidbody2D rb;
+    public float minSpeed = 50f;
+    public float maxSpeed = 150f;
+    
+    public float maxSpinSpeed = 10f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+        float randomSize = Random.Range(minSize, maxSize);
+        transform.localScale = new Vector3 (randomSize, randomSize, 1);
+
+        rb = GetComponent<Rigidbody2D>();
+        
+        float randomSpeed = Random.Range(minSpeed, maxSpeed) / randomSize;
+        Vector2 randomDirection = Random.insideUnitCircle;
+        
+        rb.AddForce(randomDirection * randomSpeed);
+        float ramdonTorque = Random.Range(-maxSpinSpeed,maxSpinSpeed);
+
+        rb.AddTorque(ramdonTorque);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
